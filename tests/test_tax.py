a7c51ae09390903445ae56954d80c6fc3e1ddb6c
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-10-07 23:25:10 +0300

    Fix assertions.

--- tests/test_tax.py
+++ tests/test_tax.py
@@ -4,17 +4,16 @@ pytest_plugins = 'pytester',
 
 
 def test_main(testdir):
-    testdir.makefile('.ini', tox="""[tox]
+    testdir.makefile('.ini', tox=r"""[tox]
 envlist = foobar
 skipsdist = true
 
 [testenv]
 commands =
-    python -c "import sys; print('sys.executable=%%r' %% sys.executable)"
-    python -c "import sys; sys.executable == %r and sys.stdout.write('OK\\n')"
-""" % (
-        sys.executable,
-    ))
+    python -c "import sys; print('sys.executable=%r' % sys.executable)"
+    python -c "import sys; sys.stdout.write('OK\n') if {0!r}.startswith(sys.executable) or sys.executable.startswith({0!r}) else sys.stdout.write('BAD\n')"
+""".format(sys.executable))
+
     result = testdir.run('tax', '-e', 'foobar')
     result.stdout.fnmatch_lines([
         "foobar create: *",
