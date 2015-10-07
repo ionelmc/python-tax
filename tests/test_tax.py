9364c518318fb09268c0de22350c07647ac99b0b
Ionel Cristian Mărieș <contact@ionelmc.ro>
2015-10-07 23:20:07 +0300

    Different assertions.

--- tests/test_tax.py
+++ tests/test_tax.py
@@ -9,14 +9,19 @@ envlist = foobar
 skipsdist = true
 
 [testenv]
-commands = python -c "import sys; assert sys.executable == %r, '%%s != %s' %% sys.executable; print('OK')"
+commands =
+    python -c "import sys; print('sys.executable=%%r' %% sys.executable)"
+    python -c "import sys; sys.executable == %r and sys.stdout.write('OK\\n')"
 """ % (
-        sys.executable, sys.executable
+        sys.executable,
     ))
     result = testdir.run('tax', '-e', 'foobar')
     result.stdout.fnmatch_lines([
         "foobar create: *",
         "foobar installed: *",
-        "foobar runtests: *",
+        "foobar runtests: PYTHONHASHSEED=*",
+        "foobar runtests: commands[[]0[]] | *",
+        "sys.executable=*",
+        "foobar runtests: commands[[]1[]] | *",
         "OK",
     ])
